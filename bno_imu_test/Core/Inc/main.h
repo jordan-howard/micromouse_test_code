caa1aa4c0e864c929891b319e9251ac12d81fcb1
Jordan Howard <jordanhoward04@yahoo.com>
2025-12-20 16:18:22 -0500

    testing imu code for bno

--- bno_imu_test/Core/Inc/main.h
+++ bno_imu_test/Core/Inc/main.h
@@ -24,7 +24,7 @@ extern "C" {
 #endif
 
 /* Includes ------------------------------------------------------------------*/
-#include "stm32f1xx_hal.h"
+#include "stm32f1xx_hal_conf.h"
 
 
 /* Exported functions prototypes ---------------------------------------------*/
--- bno_imu_test/Core/Src/main.c
+++ bno_imu_test/Core/Src/main.c
@@ -69,6 +69,7 @@ int main(void)
 
   while (1)
   {
+    return 1;
   }
 }
 
